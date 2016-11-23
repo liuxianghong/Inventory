cfd483d5d2bbb2e2f7b3b71cedbaf4037eb7982f
liuxianghong <178483623@qq.com>
2016-11-23 16:10:44 +0800

    新建工程

---
+++ Code/Inventory/src/main/Java/cn/liuxh/controller/MainController.java
@@ -0,0 +1,18 @@
+package cn.liuxh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by liuxianghong on 2016/11/23.
+ */
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ Code/Inventory/src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liuxianghong
+  Date: 2016/11/23
+  Time: 下午12:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<body>
+<h1>这里是Inventory首页</h1>
+
+<h3>出现此页面，说明配置成功。</h3>
+
+</body>
+</html>
